6a74f8c28308169324385824b2ed9fe093a71a7e
JulioRodriguez289 <78817016+JulioRodriguez289@users.noreply.github.com>
2022-03-31 23:36:13 -0400

    updated read me

--- Pods/Target Support Files/Pods-Parstagram.final-Parstagram.finalUITests/Pods-Parstagram.final-Parstagram.finalUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Parstagram_final_Parstagram_finalUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Parstagram_final_Parstagram_finalUITests
-@end
